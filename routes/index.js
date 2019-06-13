b7963be350948a14cca42b4c72af38270527b27e
Danish Shaikh <DanishS@WinjitTechnologiesPvtLtd.local>
2019-06-13 16:08:01 +0530

    another change in title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DevOps Training' });
+  res.render('index', { title: 'DevOps Training is going on' });
 });
 
 module.exports = router;
